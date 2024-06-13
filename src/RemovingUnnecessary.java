44a73624c157a35534da09b8bf3c2f62296e7f90
kriov569 <nikita.veshnyakov2014@gmail.com>
2024-06-13 14:06:53 +0300

    new repository derived a function that removes letters and spaces, as well as function to print the resulting string

--- src/RemovingUnnecessary.java
+++ src/RemovingUnnecessary.java
@@ -1,6 +1,16 @@
 public class RemovingUnnecessary {
     public static void main(String[] args) {
-            String str = "       Строка        ";
-            System.out.println(str.trim());
+        String text = "Пример текста с буквами и пробелами";
+        String cleanedText = removeLettersAndSpaces(text);
+        printResult(cleanedText);
+    }
+
+    public static String removeLettersAndSpaces(String text) {
+        String regex = "[а-яА-Яa-zA-Z\\s]";
+        return text.replaceAll(regex, "");
+    }
+
+    public static void printResult(String result) {
+        System.out.println("Результирующая строка: " + result);
     }
 }
